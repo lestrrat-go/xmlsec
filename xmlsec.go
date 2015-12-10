5dbbb30232f4ea447e6548186c903a96ce2fb0b9
Daisuke Maki <lestrrat+github@gmail.com>
2015-12-10 18:31:16 +0900

    Be a bit more pedantic about init/shutdown

--- xmlsec.go
+++ xmlsec.go
@@ -9,16 +9,44 @@ and go-libxml2 (https://github.com/lestrrat/go-libxml2)
 package xmlsec
 
 import (
+	"errors"
+	"sync"
+
 	"github.com/lestrrat/go-xmlsec/clib"
 )
 
-// Init calls various initilization functions from xmlsec1
+var initLock = sync.Mutex{}
+var initialized bool
+
+// Init calls various initilization functions from xmlsec1. This MUST be
+// called prior to doing any real signing/encryption using xmlsec
 func Init() error {
-	return clib.XMLSecInit()
+	initLock.Lock()
+	defer initLock.Unlock()
+
+	if initialized {
+		return errors.New("xmlsec already initialized")
+	}
+
+	if err := clib.XMLSecInit(); err != nil {
+		return err
+	}
+	initialized = true
+	return nil
 }
 
-// Shutdown calls various cleanup functions from xmlsec1
+// Shutdown calls various cleanup functions from xmlsec1. This MUST be
+// called when you are no longer using xmlsec
 func Shutdown() error {
-	return clib.XMLSecShutdown()
-}
+	initLock.Lock()
+	defer initLock.Unlock()
 
+	if !initialized {
+		return errors.New("xmlsec has not been initialized")
+	}
+	if err := clib.XMLSecShutdown(); err != nil {
+		return err
+	}
+	initialized = false
+	return nil
+}
